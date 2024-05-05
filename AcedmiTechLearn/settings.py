f910ac0be00b4190ada56d912b4ec4026dee0541
talebSleiman <talebsleiman4@gmail.com>
2024-05-05 13:51:21 +0300

    change the class card design

--- AcedmiTechLearn/settings.py
+++ AcedmiTechLearn/settings.py
@@ -175,7 +175,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ahmadsleiman562@gmail.com'
-EMAIL_HOST_PASSWORD = 'ghpl gnty jsgm nxqs'
+EMAIL_HOST_PASSWORD = 'hhig joky zpnj ybdb'
 EMAIL_USE_TLS = True
 
 JAZZMIN_SETTINGS = {
